6750e0515d46caa2c6f90010c68ffd2dc5a3caac
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2023-11-28 15:57:41 +0100

    Implementing Verification Email Page

--- lib/screens/verificationEmailPage.dart
+++ lib/screens/verificationEmailPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meet_pe/screens/singupPage.dart';
+
+import '../utils/utils.dart';
 
 class VerificationEmailPage extends StatefulWidget {
   const VerificationEmailPage({super.key});
@@ -8,8 +12,128 @@ class VerificationEmailPage extends StatefulWidget {
 }
 
 class _VerificationEmailPageState extends State<VerificationEmailPage> {
+  bool _isError = false;
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 88,
+          left: 48,
+          right: 48,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: _textController,
+              style: const TextStyle(
+                color: Color(0xFF1D1D1D),
+                fontSize: 14,
+                fontFamily: 'Outfit',
+                fontWeight: FontWeight.w500,
+                height: 0.10,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: const TextStyle(
+                  color: Color(0xFF737271),
+                  fontSize: 14,
+                  fontFamily: 'Outfit',
+                  fontWeight: FontWeight.w300,
+                  height: 0.10,
+                ),
+                contentPadding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+                // Adjust padding
+                suffix: const SizedBox(height: 10),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: _isError ? Color(0xFFFF0000) : Color(0xFF737271)),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: _isError ? Color(0xFFFF0000) : Color(0xFF737271)),
+                ),
+              ),
+              //autofocus: true,
+              //textInputAction: TextInputAction.next,
+              //validator: AppResources.validatorNotEmpty,
+              //controller: bloc.usernameController,
+              onChanged: (value) {
+                // Validate email or any other criteria
+                if (value.isNotEmpty && !value.contains('@')) {
+                  setState(() {
+                    _isError = true;
+                  });
+                } else {
+                  setState(() {
+                    _isError = false;
+                  });
+                }
+              },
+            ),
+            SizedBox(
+              height: 17,
+            ),
+            Text(
+              'CHAMPS INVALIDE',
+              style: TextStyle(
+                color: Color(0xFFFF0000),
+                fontSize: 10,
+                fontFamily: 'Outfit',
+                fontWeight: FontWeight.w400,
+                height: 0.14,
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Container(
+                    margin: const EdgeInsets.all(5),
+                    width: double.infinity,
+                    child: SizedBox(
+                      width: 183,
+                      height: 44,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.all(Size(183, 44)),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.symmetric(horizontal: 34, vertical: 14)),
+                          backgroundColor: MaterialStateProperty.all(Color(0xFFFF4C00)),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          navigateTo(context, (_) => const SingUpPage());
+                        },
+                        child: Image.asset(
+                          'images/arrowLongRight.png',
+                          fit: BoxFit.contain,
+                          width: 44,
+                          height: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
