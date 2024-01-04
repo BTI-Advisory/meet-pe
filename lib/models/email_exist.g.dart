5978696463f4878463ce806df628fc57b4bf08cc
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2024-01-04 11:46:17 +0100

    Implementing verification email API.

---
+++ lib/models/email_exist.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'email_exist.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EmailExist _$EmailExistFromJson(Map<String, dynamic> json) => EmailExist(
+  exist: json['msg'] as String,
+);
