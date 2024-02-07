1f12b163bacc71c9fab49e162e70c2d45714fc4c
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2024-02-07 11:41:41 +0100

    Implementing UI in step 7 UI.

---
+++ lib/screens/onBoardingPages/guide/create_experience/create_exp_step8.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateExpStep8 extends StatefulWidget {
+  const CreateExpStep8({super.key});
+
+  @override
+  State<CreateExpStep8> createState() => _CreateExpStep8State();
+}
+
+class _CreateExpStep8State extends State<CreateExpStep8> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
