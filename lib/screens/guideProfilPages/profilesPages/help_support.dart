35ed85a65575362c0ddfc8e610352684be939d15
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2024-02-15 14:33:55 +0100

    Implementing Help and Support UI.

---
+++ lib/screens/guideProfilPages/profilesPages/help_support.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import '../../../resources/resources.dart';
+import '../../../utils/responsive_size.dart';
+import '../../../widgets/_widgets.dart';
+
+class HelpSupport extends StatefulWidget {
+  const HelpSupport({super.key});
+
+  @override
+  State<HelpSupport> createState() => _HelpSupportState();
+}
+
+class _HelpSupportState extends State<HelpSupport> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const EpAppBar(
+        title: 'Aide & Assistance',
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: ResponsiveSize.calculateWidth(31, context)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Aide',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(
+                        fontSize: 20, color: AppResources.colorDark),
+                  ),
+                  const SizedBox(height: 17),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: AppResources.colorGray30),
+                  ),
+                  const SizedBox(height: 30),
+                  blocHelp(),
+                  blocHelp(),
+                  blocHelp(),
+                  blocHelp(),
+                  blocHelp(),
+                  blocHelp(),
+                  const SizedBox(height: 15),
+                  Text(
+                    'Assistance',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(
+                        fontSize: 20, color: AppResources.colorDark),
+                  ),
+                  const SizedBox(height: 17),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: AppResources.colorGray30),
+                  ),
+                  const SizedBox(height: 30),
+                  blocHelp(),
+                  blocHelp(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget blocHelp() {
+    return Column(
+      children: [
+        const SizedBox(height: 19),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Lorem',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w400, color: const Color(0xFF797979)),
+            ),
+            Image.asset('images/chevron_right.png',
+                width: 27, height: 27, fit: BoxFit.fill),
+          ],
+        ),
+        const SizedBox(height: 19),
+        Container(
+          width: 390,
+          decoration: const ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                width: 1,
+                strokeAlign: BorderSide.strokeAlignCenter,
+                color: AppResources.colorImputStroke,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
