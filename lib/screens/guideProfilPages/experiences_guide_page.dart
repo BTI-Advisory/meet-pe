f305169319f1b593600e913b04628f8490365791
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2024-02-16 18:02:58 +0100

    Implementing Request in experiences guide UI.

--- lib/screens/guideProfilPages/experiences_guide_page.dart
+++ lib/screens/guideProfilPages/experiences_guide_page.dart
@@ -12,6 +12,7 @@ class ExperiencesGuidePage extends StatefulWidget {
 
 class _ExperiencesGuidePageState extends State<ExperiencesGuidePage> {
   bool isRequest = true; // Track if it's currently "Request" or "Experience"
+  bool accepted = false;
 
   void toggleRole() {
     setState(() {
@@ -48,7 +49,19 @@ class _ExperiencesGuidePageState extends State<ExperiencesGuidePage> {
                 ],
               ),
             ),
-            buildToggleButtons()
+            buildToggleButtons(),
+            const SizedBox(height: 40),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: ResponsiveSize.calculateWidth(13, context)),
+              child: Column(
+                children: [
+                  requestCard(),
+                  const SizedBox(height: 19),
+                  requestCard(),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -124,6 +137,130 @@ class _ExperiencesGuidePageState extends State<ExperiencesGuidePage> {
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  Widget requestCard() {
+    return Column(
+      children: [
+        Container(
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            shadows: const [
+              BoxShadow(
+                color: Color(0x1E000000),
+                blurRadius: 12,
+                offset: Offset(0, 4),
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const SizedBox(width: 10),
+                  Column(
+                    children: [
+                      ClipOval(
+                        child: Image.asset('images/imageTest.png', width: 38, height: 38, fit: BoxFit.cover)
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        'Lucie',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodySmall
+                            ?.copyWith(fontWeight: FontWeight.w500, color: AppResources.colorDark),
+                      )
+                    ],
+                  ),
+                  const SizedBox(width: 17),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Ma. 17 fév. à 11h30',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(fontSize: 14, color: AppResources.colorDark),
+                      ),
+                      Text(
+                        'Le Paris de Maria...',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: AppResources.colorGray60),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 23),
+                  Visibility(
+                    visible: !accepted,
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              accepted = true;
+                            });
+                            print('Accepted');
+                          },
+                          child: Column(
+                            children: [
+                              Icon(Icons.check, size: 24,),
+                              const SizedBox(height: 4),
+                              Text(
+                                'Accepter',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.copyWith(color: AppResources.colorDark),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 19),
+                        Column(
+                          children: [
+                            Icon(Icons.do_not_disturb, size: 24,),
+                            const SizedBox(height: 4),
+                            Text(
+                              'Refuser',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(color: AppResources.colorDark),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Visibility(
+                    visible: accepted,
+                    child: Row(
+                      children: [
+                        const Icon(Icons.check, size: 24, color: Color(0xFF54EE9D),),
+                        const SizedBox(width: 8),
+                        Text(
+                          'Accepté',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge
+                              ?.copyWith(color: Color(0xFF54EE9D)),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 11),
+            ],
+          ),
+        ),
+        const SizedBox(height: 30),
       ],
     );
   }
