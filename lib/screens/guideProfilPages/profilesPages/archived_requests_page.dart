9d194d7f03804b3c0823d47b467a43a007e60654
bahaeddine.boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2024-02-21 17:43:12 +0100

    Add mask in archived request and card experience UI.

--- lib/screens/guideProfilPages/profilesPages/archived_requests_page.dart
+++ lib/screens/guideProfilPages/profilesPages/archived_requests_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_mask/widget_mask.dart';
 
 import '../../../resources/resources.dart';
 import '../../../utils/responsive_size.dart';
@@ -57,7 +58,21 @@ class _ArchivedRequestsPageState extends State<ArchivedRequestsPage> {
           ),
           child: Row(
             children: [
-              Image.asset('images/imageTest.png', width: 68, height: 68, fit: BoxFit.cover),
+              WidgetMask(
+                blendMode: BlendMode.srcATop,
+                childSaveLayer: true,
+                mask: Image.asset(
+                  'images/imageTest.png',
+                  width: 68,
+                  height: 68,
+                  fit: BoxFit.cover,
+                ),
+                child: Image.asset(
+                  'images/mask_picture.png',
+                  width: 68,
+                  height: 68,
+                ),
+              ),
               const SizedBox(width: 16),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
